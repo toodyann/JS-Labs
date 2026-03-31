90152f20e805533299798532f39f59ad44d5259f
Konovalov Anatolii <mrdenth3@gmail.com>
2026-03-31 13:58:59 +0300

    feat: Lab3

---
+++ src/Lab3/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function (window) {
+  var speakWord = "Goodbye";
+
+  var byeSpeaker = {};
+
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.byeSpeaker = byeSpeaker;
+})(window);
